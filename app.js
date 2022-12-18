78f44af47e787d53e4c1067da82a8b02ed7d16e9
i-guerrero <isaacguerrero@pursuit.org>
2022-12-18 16:06:11 -0500

    Set up logs controller

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // DEPENDENCIES
 const express = require("express");
+const logsController = require("./controllers/logsController");
 
 //CONFIGURATION
 const app = express();
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Welcome to the captain's log");
+});
+
+app.use("/logs", logsController);
+
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Sorry, page not found" });
 });
 
 // EXPORT
