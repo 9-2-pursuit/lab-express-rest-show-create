49d89bdfb65e50b9b597e81ab82e3e1dac31d199
Karyn Huston <huston.karyn@gmail.com>
2022-12-19 14:23:18 -0500

    deletes at index

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -11,18 +11,26 @@ logs.get("/", (req, res) => {
 
 //SHOW, GET, READ
 logs.get("/:id", (req, res) => {
-const id = Number(req.params.id)
-if(logArray[id]) {
-    res.json(logArray[id])
-}else {
-    res.redirect(404)
-}
+  const id = Number(req.params.id);
+  if (logArray[id]) {
+    res.json(logArray[id]);
+  } else {
+    res.redirect(404);
+  }
 });
 
 //CREATE, POST, CEATE
 logs.post("/", (req, res) => {
   logArray.push(req.body);
   res.json(logArray[logArray.length - 1]);
+});
+
+//UPDATE, DELETE
+
+logs.delete("/:id", (req, res) => {
+let index = logArray.findIndex(item => item.id === req.params.id);
+res.json(logArray.splice(index, 1))
+
 });
 
 module.exports = logs;
