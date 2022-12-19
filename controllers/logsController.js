5a2a29cb378908f95fa1a9c27cf3fe7425b7aa4c
Karyn Huston <huston.karyn@gmail.com>
2022-12-19 13:40:45 -0500

    post, get, redicrect

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,21 +1,28 @@
 const express = require("express");
-const logArray = require("../models/log.js")
+const logArray = require("../models/log.js");
 
-
-//INSTINTIATE ROUTING 
+//INSTINTIATE ROUTING
 const logs = express.Router();
 
-
+//INDEX, GET, READ
 logs.get("/", (req, res) => {
-    res.json(logArray)
-})
+  res.json(logArray);
+});
 
+//SHOW, GET, READ
+logs.get("/:id", (req, res) => {
+const id = Number(req.params.id)
+if(logArray[id]) {
+    res.json(logArray[id])
+}else {
+    res.redirect(404)
+}
+});
 
-
-
-
-
-
-
+//CREATE, POST, CEATE
+logs.post("/", (req, res) => {
+  logArray.push(req.body);
+  res.json(logArray[logArray.length - 1]);
+});
 
 module.exports = logs;
