4ad22e5e4e78bc7cddd7c93f4350692c95749ef2
i-guerrero <isaacguerrero@pursuit.org>
2022-12-19 10:06:30 -0500

    Add update and delete error handling

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -65,8 +65,22 @@ logs.post("/", validateLog, (req, res) => {
 
 // DELETE
 logs.delete("/:id", (req, res) => {
-  const deletedLog = logsArray.splice(req.params.id, 1);
-  res.json(deletedLog);
+  if (logsArray[req.params.id]) {
+    const deletedLog = logsArray.splice(req.params.id, 1);
+    res.status(200).json(deletedLog);
+  } else {
+    res.status(404).json({ error: "Log not found" });
+  }
+});
+
+// UPDATE
+logs.put("/:id", (req, res) => {
+  if (logsArray[req.params.id]) {
+    logsArray[req.params.id] = req.body;
+    res.status(200).json(logsArray[req.params.id]);
+  } else {
+    res.status(404).json({ error: "Log not found" });
+  }
 });
 
 module.exports = logs;
